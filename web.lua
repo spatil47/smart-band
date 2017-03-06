9597bfc6018cccc3391df9dfe8defdfa990aff03
Siddharth Patil <sp899@njit.edu>
2017-03-06 18:30:04 -0500

    Partially-working smart outlet control.
    The NodeMCU switches the smart outlet on when it
    receives a datagram on UDP port 5000.
    It cannot switch the smart outlet off.

--- web.lua
+++ web.lua
@@ -1,17 +1,9 @@
-srv=net.createServer(net.TCP)
-srv:listen(8000,function(conn)
-  conn:on("receive",function(conn,payload)
-    print(payload)
-    local buf = "HTTP/1.1 200 OK\r\n"
-    buf = buf.."Content-Type: text/html; charset=utf-8\r\n\r\n"
-    buf = buf.."<html><head><title>SPS Web Interface</title></head><body>"
-    buf = buf.."<button onClick=\"show()\">IP address</button><br><br>"
-    buf = buf.."<span id='ip' style='visibility:hidden;font-family:monospace'>"
-    buf = buf..wifi.sta.getip()
-    buf = buf.."</span>"
-    buf = buf.."<script>function show() { document.getElementById('ip').style.visibility = 'visible'; }</script>"
-    buf = buf.."</body></html>"
-    conn:send(buf)
-  end)
-  conn:on("sent",function(conn) conn:close() end)
+udpSocket = net.createUDPSocket()
+udpSocket:listen(5000)
+udpSocket:on("receive", function(s, data, port, ip)
+    print(string.format("received"))
+    cmd = file.open("Smart_outlet_on.dat", "r")
+    s:send(80, "192.168.1.100", cmd:read())
 end)
+port, ip = udpSocket:getaddr()
+print(string.format("local UDP socket address / port: %s:%d", ip, port))
