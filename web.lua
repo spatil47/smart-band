f8361fc301aeb0d2943a2c62df7e1c4345955e05
Siddharth Patil <sp899@njit.edu>
2017-03-24 13:46:47 -0400

    Trying to send smart outlet commands based on GPIO callbacks
    GPIO1 and GPIO10 will send smart outlet commands when a rising-edge
    interrupt triggers the respective callback.

--- web.lua
+++ web.lua
@@ -1,16 +1,24 @@
+target = "192.168.43.118"
+
+gpio.mode(1,gpio.INT,gpio.PULLUP)
+gpio.mode(10,gpio.INT,gpio.PULLUP)
+
 udpSocket = net.createUDPSocket()
 udpSocket:listen(5000)
-udpSocket:on("receive", function(s, data, port, ip)
-    print(string.format("received"))
-    if(data=="on") then
-        cmd = file.open("Smart_outlet_on.dat", "r")
-        s:send(80, "192.168.1.100", cmd:read())
-        cmd.close()
-    elseif(data=="off") then
-        cmd = file.open("Smart_outlet_off.dat", "r")
-        s:send(80, "192.168.1.100", cmd:read())
-        cmd.close()
-    end
+
+gpio.trig(1,"up",function(level,when)
+    print("on")
+    cmd = file.open("Smart_outlet_on.dat", "r")
+    udpSocket:send(80, target, cmd:read())
+    cmd.close()
 end)
+
+gpio.trig(10,"up",function(level,when)
+    print("off")
+    cmd = file.open("Smart_outlet_off.dat", "r")
+    udpSocket:send(80, target, cmd:read())
+    cmd.close()
+end)
+
 port, ip = udpSocket:getaddr()
 print(string.format("local UDP socket address / port: %s:%d", ip, port))
