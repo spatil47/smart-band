6fa03f5f221830c99285ff099fec5fffed444fc8
Siddharth Patil <sp899@njit.edu>
2017-03-24 10:50:13 -0400

    NodeMCU able to send smart outlet commands through callbacks
    Upon receiving simple "on"/"off" plaintext commands, the NodeMCU will
    send the corresponding command to the smart outlet. It is currently not
    possible in this code to send the smart outlet commands outside of the
    callback function.

--- web.lua
+++ web.lua
@@ -2,8 +2,15 @@ udpSocket = net.createUDPSocket()
 udpSocket:listen(5000)
 udpSocket:on("receive", function(s, data, port, ip)
     print(string.format("received"))
-    cmd = file.open("Smart_outlet_on.dat", "r")
-    s:send(80, "192.168.1.100", cmd:read())
+    if(data=="on") then
+        cmd = file.open("Smart_outlet_on.dat", "r")
+        s:send(80, "192.168.1.100", cmd:read())
+        cmd.close()
+    elseif(data=="off") then
+        cmd = file.open("Smart_outlet_off.dat", "r")
+        s:send(80, "192.168.1.100", cmd:read())
+        cmd.close()
+    end
 end)
 port, ip = udpSocket:getaddr()
 print(string.format("local UDP socket address / port: %s:%d", ip, port))
